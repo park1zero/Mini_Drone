0c11c5a75f05c1b8adae3e81a3f2e4076c025f0a
kjsoo0146 <110090496+kjsoo0146@users.noreply.github.com>
2023-03-17 18:27:07 +0900

    .
    .

---
+++ Team_CDSL/variables.m
@@ -0,0 +1,19 @@
+%% frame size
+FRAME_SIZE_HEIGHT=120;
+FRAME_SIZE_WIDTH=160;
+COG_X=FRAME_SIZE_HEIGHT/2;
+COG_Y=FRAME_SIZE_WIDTH/2;
+
+%% binarizer
+BINARIZER_THRESHOLD = 100;
+G_B_GAIN = 2;
+
+%% 4dots picker
+S = 80;
+
+%% Landing mark detector
+DISK_KERNEL = 11;
+
+%% k-means algorithm
+
+
