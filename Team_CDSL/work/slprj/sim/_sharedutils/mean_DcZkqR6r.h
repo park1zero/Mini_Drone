eaf86ee3abb216cc0f0e0e85a21099bb0ad142e8
wonyoung <450time@naver.com>
2023-03-28 15:26:24 +0900

    03.28
    control part, variable modify

--- Team_CDSL/work/slprj/sim/_sharedutils/mean_DcZkqR6r.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTW_HEADER_mean_DcZkqR6r_h_
-#define RTW_HEADER_mean_DcZkqR6r_h_
-#include "rtwtypes.h"
-#include "multiword_types.h"
-
-extern void mean_DcZkqR6r(const real_T x[38400], real_T y[2]);
-
-#endif
--- Team_CDSL/work/slprj/sim/_sharedutils/mean_FUHoqmNL.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTW_HEADER_mean_FUHoqmNL_h_
-#define RTW_HEADER_mean_FUHoqmNL_h_
-#include "rtwtypes.h"
-#include "multiword_types.h"
-
-extern void mean_FUHoqmNL(const real_T x[2400], real_T y[2]);
-
-#endif
